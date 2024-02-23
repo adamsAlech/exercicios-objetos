db63139ee07ca075e9beef1cb987d152b8861c33
Adams Lech <adams.a.lech@hotmail.com>
2024-02-23 20:14:38 -0300

    up exercicio 02

---
+++ 03/index.js
@@ -0,0 +1,19 @@
+/*Exercício 03
+
+## Mensagem de apresentação
+
+1. Dado o objeto `usuario` abaixo, monte a mensagem `Olá... Eu sou João e tenho 25 anos de idade. 
+Sou programador e minha cor preferida é verde!`, passando dinamicamente as informações contidas no objeto.
+*/
+
+
+const usuario = {
+    nome: "Lucas",
+    idade: 32,
+    profissao: "programador",
+    cor_preferida: "preto",
+};
+
+const { nome, idade, profissao, cor_preferida } = usuario;
+
+console.log(`Olá, eu sou ${nome}, tenho ${idade} de idade, sou ${profissao} e minha cor favorita é ${cor_preferida}.`)
