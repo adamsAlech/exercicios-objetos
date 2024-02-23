026563ac80def26bdf120765b2e3c31f435cfdec
Adams Lech <adams.a.lech@hotmail.com>
2024-02-23 20:50:13 -0300

    up exercicio 04

---
+++ 04/index.js
@@ -0,0 +1,46 @@
+/*Exercício 04
+
+## Adicionando maior idade
+
+1. Dado a lista de objetos abaixo, adicione uma nova propriedade chamada `maior_idade` 
+para cada objeto da lista. A propriedade deverá receber um valor booleano de acordo 
+com a propriedade `idade` de cada objeto. Caso a idade seja maior que 17, o valor deve 
+ser `true`, caso contrário `false`.*/
+
+const usuarios = [
+    {
+        nome: "João",
+        idade: 25,
+    },
+    {
+        nome: "Ana",
+        idade: 18,
+    },
+    {
+        nome: "Beatriz",
+        idade: 15,
+    },
+    {
+        nome: "Carlos",
+        idade: 16,
+    },
+    {
+        nome: "Antonio",
+        idade: 32,
+    },
+];
+let maiorIdade = '';
+for (let i = 0; i < usuarios.length; i++) {
+    if (usuarios[i].idade >= 18) {
+        usuarios[i].maiorIdade = true;
+    } else {
+        usuarios[i].maiorIdade = false;
+    }
+};
+
+const objetosFundidos = {
+    ...usuarios,
+    ...maiorIdade,
+};
+
+console.log(objetosFundidos);
